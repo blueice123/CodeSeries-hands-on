edae444fd7f203b3fa9a8a267870aeb60c024afc
하수용 <syha@mz.co.kr>
2020-06-12 13:05:48 +0900

    initial commit

--- flask-example/01-hello-world/hello.py
+++ flask-example/01-hello-world/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello, World!</h1>"
+    return "<h1>Hello, World!, Bule Deployment</h1>"
 
 @app.route('/user/<name>')
 def user(name):
