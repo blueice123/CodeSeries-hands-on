6c435d56a29893ef5d361b1e75858a5b21b9c870
하수용 <syha@mz.co.kr>
2020-06-13 19:43:16 +0900

    test

--- terraform-example/s3_main.tf
+++ terraform-example/s3_main.tf
@@ -2,6 +2,7 @@ resource "aws_s3_bucket" "s3_for_artifact" {
   bucket = "${var.company}-${var.environment}-artifact-${random_id.random.hex}"
   region = var.region
   acl    = "private"
+  force_destroy = true
   tags = {
     Name = format(
       "%s-%s-artifact",
