bd1ed935c79ef129afa9469ec19506e2a66bd817
하수용 <syha@mz.co.kr>
2020-06-16 12:46:16 +0900

    test

--- flask-example/01-hello-world/scripts/start_app.sh
+++ flask-example/01-hello-world/scripts/start_app.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-sudo python3 /home/ec2-user/flask-example/01-hello-world/hello.py &
-
+sudo python3 /home/ec2-user/flask-example/01-hello-world/hello.py >/dev/null 2>&1 &
+echo "0"
--- flask-example/01-hello-world/scripts/stop_app.sh
+++ flask-example/01-hello-world/scripts/stop_app.sh
@@ -3,4 +3,7 @@
 isExistApp=`ps -ef|grep python| grep -v grep`
 if [[ -n  $isExistApp ]]; then
     sudo kill -9 $(ps -ef |grep hello.py |grep -v grep | awk '{print $2}')
+    echo "0"
+else 
+    echo "0"
 fi
