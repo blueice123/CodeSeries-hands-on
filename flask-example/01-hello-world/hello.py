c0875778f26eec86fda1554718ea74a77456327d
하수용 <syha@mz.co.kr>
2020-06-20 17:11:03 +0900

    test

--- flask-example/01-hello-world/hello.py
+++ flask-example/01-hello-world/hello.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>MEGAZONE CLOUD - CI/CD Hands-on TEST</h1>"
+    return "<h1>MEGAZONE CLOUD - CI/CD Hands-on</h1>"
 
 @app.route('/user/<name>')
 def user(name):
